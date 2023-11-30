6d02b9635a3a9d840424dab1081c5e6049418dd0
tosantos <thiago.santos83@gmail.com>
2023-11-30 12:10:17 +0000

    Add SLF4J dependency and implementation

---
+++ recipes-cli/src/main/java/org/olivetree/recipes/cli/RecipesCliApplication.java
@@ -0,0 +1,12 @@
+package org.olivetree.recipes.cli;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class RecipesCliApplication {
+
+    private static final Logger LOG = LoggerFactory.getLogger(RecipesCliApplication.class);
+    public static void main(String[] args) {
+        LOG.info("Recipes CLI Application started");
+    }
+}
---
+++ recipes-client/src/main/java/org/olivetree/recipes/client/RecipesClient.java
@@ -0,0 +1,13 @@
+package org.olivetree.recipes.client;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class RecipesClient {
+
+    private static final Logger LOG = LoggerFactory.getLogger(RecipesClient.class);
+
+    public static void main(String[] args) {
+        LOG.info("Recipes REST API Client Application started");
+    }
+}
